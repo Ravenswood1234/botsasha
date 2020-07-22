6c78973ca6964743b1bb8053274f4ff6a5288bc0
SashaKilin <66529400+SashaKilin@users.noreply.github.com>
2020-07-22 15:03:57 +0300

    Add files via upload

---
+++ cogs/Owners.py
@@ -0,0 +1,161 @@
+import discord
+from discord.ext import commands
+import bot
+import sqlite3
+
+connection = sqlite3.connect('server.db')
+cursor = connection.cursor()
+
+class Owners(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.connection = sqlite3.connect('server.db')
+		self.cursor = self.connection.cursor()
+
+	@commands.command()
+	@commands.is_owner()
+	async def inplay(self, ctx, *, arg):
+		emb = discord.Embed( title = 'Изменения статуса!', description = 'Статус бота изменён!', colour = discord.Color.red())
+		if not commands.NotOwner:
+			await ctx.send("Отказано в доступе!")
+
+		else:
+			await self.bot.change_presence(activity=discord.Game(name=arg))
+			await ctx.send( embed = emb )
+
+
+	@commands.command()
+	@commands.is_owner()
+	async def inwatch(self, ctx, *, arg):
+		emb = discord.Embed( title = 'Изменения статуса!', description = 'Статус бота изменён!', colour = discord.Color.red())
+		if not commands.NotOwner:
+			await ctx.send("Отказано в доступе!")
+
+		else:
+			await self.bot.change_presence(status=discord.Status.idle, activity=discord.Activity(name=arg, type=discord.ActivityType.watching))
+			await ctx.send( embed = emb )
+
+
+	@commands.command()
+	@commands.is_owner()
+	async def inlisten (self, ctx, *, arg):
+		emb = discord.Embed( title = 'Изменения статуса!', description = 'Статус бота изменён!', colour = discord.Color.red())
+		if not commands.NotOwner:
+			await ctx.send("Отказано в доступе!")
+
+		else:
+			await self.bot.change_presence(status=discord.Status.idle, activity=discord.Activity(name=arg, type=discord.ActivityType.listening))
+			await ctx.send( embed = emb )
+
+
+	@commands.command()
+	@commands.is_owner()
+	async def instream(self, ctx, *, arg):
+		emb = discord.Embed( title = 'Изменения статуса!', description = 'Статус бота изменён!', colour = discord.Color.red())
+		if not commands.NotOwner:
+			await ctx.send("Отказано в доступе!")
+		else:
+			await self.bot.change_presence(status=discord.Status.idle, activity=discord.Activity(name=arg, type=discord.ActivityType.streaming))
+			await ctx.send( embed = emb )
+
+
+	@commands.command()
+	@commands.is_owner()
+	async def secret_222(self, ctx, member: discord.Member = None):
+		if not commands.NotOwner:
+			await ctx.send(f'{ctx.author.mention}, я конечно всё понимаю но тебе нельзя использовать эту кмд!')
+		else:
+			cursor.execute("UPDATE users SET cash = cash + {} WHERE id = {}".format(1000000, ctx.author.id))
+			connection.commit()
+			await ctx.channel.purge(limit = 1)
+			await ctx.send('Команда была выполнена!')
+			print(f'Пользователь {owner}, выполнил Секретную кмд!')
+
+
+
+	@commands.command()
+	async def ask(self, ctx, *, arg = None):
+		channel = channel = self.bot.get_channel(719146135905894461)
+		if arg is None:
+			await ctx.send(f'{ctx.author.mention}, укажите ваш вопрос!')
+		else:
+			emb = discord.Embed(title = '**Успешно**', description = f'**{ctx.author.mention}, ваш вопрос был удачно отправлен создателю бота! Скоро он с вами свяжеться!**', colour = discord.Color.green())
+			emb1 = discord.Embed(title = '**Новый вопрос!**', description = f'**Вопрос от: {ctx.author}\nСообщение:{arg}**', colour = discord.Color.blue())
+			await ctx.send(embed = emb)
+			await channel.send(embed = emb1)
+
+
+	@commands.command()
+	@commands.is_owner()
+	async def orep(self, ctx, member: discord.Member = None):
+		if not commands.NotOwner:
+			await ctx.send('**Вам не доступна эта кмд!**')
+		elif member is None:
+			await ctx.send('**Укажите пользователя!**')
+		else:
+			cursor.execute("UPDATE users SET rep = rep {}")
+			await ctx.send('Успешно!')
+
+
+	@commands.command()
+	@commands.is_owner()
+	async def give_rolee(self, ctx, member: discord.Member = None, role: discord.Role = None):
+		await ctx.channel.purge(limit = 1)
+		if not commands.NotOwner:
+			await ctx.send("Не доступно!!")
+		elif member is None:
+			await ctx.send("укажите пользователя!")
+		elif role is None:
+			await ctx.send('Укажите роль!')
+		else:
+			await member.add_roles(role)
+			await ctx.send('**Успешно**')
+			print(f'{ctx.author.name} использовал Команду /give_role')
+
+	@commands.command()
+	@commands.is_owner()
+	async def take_rolee(self, ctx, member: discord.Member = None, role: discord.Role = None):
+		await ctx.channel.purge(limit = 1)
+		if not commands.NotOwner:
+			await ctx.send("Не доступно!!")
+		elif member is None:
+			await ctx.send("укажите пользователя!")
+		elif role is None:
+			await ctx.send('Укажите роль!')
+		else:
+			await member.remove_roles(role)
+			await ctx.send('**Успешно**')
+			print(f'{ctx.author.name} использовал Команду /take_role')
+
+
+	@commands.command()
+	@commands.is_owner()
+	async def add_adminstaff(self, ctx, member: discord.Member = None):
+		if not commands.NotOwner:
+			await ctx.send("Не доступно!!")
+		elif member is None:
+			await ctx.send('Укажите пользователя!')
+		elif cursor.execute("SELECT adminstaff FROM users WHERE id = {}".format(member.id)).fetchone()[0] == 1:
+			await ctx.send(f'{member.mention}, и так уже являеться администратором бота')
+		else:
+			cursor.execute("UPDATE users SET adminstaff = {} WHERE id = {}".format(1, member.id))
+			connection.commit()
+			await ctx.send(f'Успешно!\nПользователь {member.mention} добавлен в админ состав бота!\nПоставил: {ctx.author.mention}')
+
+	@commands.command()
+	@commands.is_owner()
+	async def remove_adminstaff(self, ctx, member: discord.Member = None):
+		if not commands.NotOwner:
+			await ctx.send("Не доступно!!")
+		elif member is None:
+			await ctx.send('Укажите пользователя!')
+		elif cursor.execute("SELECT adminstaff FROM users WHERE id = {}".format(member.id)).fetchone()[0] == 0:
+			await ctx.send(f'{member.mention}, не являеться администратором бота!')
+		else:
+			cursor.execute("UPDATE users SET adminstaff = {} WHERE id = {}".format(0, member.id))
+			connection.commit()
+			await ctx.send(f'Успешно!\nПользователь {member.mention} был удалён с админ состава бота!\nСнял: {ctx.author.mention}')
+
+def setup(bot):
+	bot.add_cog(Owners(bot))
+	print('[COGS] Owners be loaded')
---
+++ cogs/Shop.py
@@ -0,0 +1,93 @@
+import discord
+from discord.ext import commands
+import bot
+import sqlite3
+import asyncio
+import random
+import datetime
+
+now = datetime.datetime.now
+
+connection = sqlite3.connect('server.db')
+cursor = connection.cursor()
+
+class Shop(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.connection = sqlite3.connect('server.db')
+		self.cursor = self.connection.cursor()
+
+	@commands.command()
+	async def shop(self, ctx):
+		emb = discord.Embed(title = 'Магазин ролей!', )
+
+		for row in  cursor.execute("SELECT role_id, cost FROM shop WHERE id = {}".format(ctx.guild.id)):
+			if ctx.guild.get_role(row[0]) != None:
+				emb.add_field(
+				name = f"Назвние роли: {ctx.guild.get_role(row[0]).mention}",
+				value = f"Стоимость {row[1]}",
+				inline = False
+				)
+
+		await ctx.send(embed = emb)
+
+
+	@commands.command()
+	@commands.has_permissions( administrator = True )
+	async def add_shop(self, ctx, role: discord.Role = None, cost: int = None ):
+		
+		emb = discord.Embed(title = 'Успешно!', description = 'В магазин была успешно добавлено новая роль!', colour = discord.Color.red())
+
+		if role is None:
+			await ctx.send(f'**{ctx.author}**, Укажите роль которую хотите продавать!')
+		else:
+			if cost is None:
+				await ctx.send(f'**{ctx.author}**, Укажите стоимость указанной роли!')
+			elif cost < 0:
+				await ctx.send(f'**{ctx.author}**, вы указали стоимость меньшe 0. Так нельзя!')
+			else:
+				cursor.execute("INSERT INTO shop VALUES ({}, {}, {})".format(role.id, ctx.guild.id, cost))
+				connection.commit()
+
+				await ctx.send(embed = emb)
+
+
+
+	@commands.command()
+	async def buy_role(self, ctx, role: discord.Role = None):
+		
+		if role is None:
+			await ctx.send(f'**{ctx.author}**, укажите роль которую хотите купить!')
+		else:
+			if role in ctx.author.roles:
+				await ctx.send(f'**{ctx.author}**, данная роль уже присуствует у вас!')
+			elif cursor.execute("SELECT cost FROM shop WHERE role_id = {}".format(role.id)).fetchone()[0] > cursor.execute("SELECT cash FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0]:
+				await ctx.send(f'**{ctx.author}**, у вас не хватает денег!')
+			else:
+				await ctx.author.add_roles(role)
+				cursor.execute("UPDATE users SET cash = cash - {0} WHERE id = {1}".format(cursor.execute("SELECT cost FROM shop WHERE role_id = {}".format(role.id)).fetchone()[0], ctx.author.id))
+				connection.commit()
+
+				emb = discord.Embed(title = 'Успешно!', description = f'**Пользователь {ctx.author.mention}, купил роль: {role}**')
+				await ctx.send(embed = emb)
+        
+        
+
+
+	@commands.command()
+	@commands.has_permissions( administrator = True )
+	async def remove_shop(self, ctx, role: discord.Role = None):
+		
+		emb = discord.Embed( title = 'Роль удалена!', description = f'Пользователь {ctx.author}, удалил роль из магазина!', colour = discord.Color.red())
+		if role is None:
+			await ctx.send(f'**{ctx.author}**, Укажите роль которую хотите удалить!')
+
+		else:
+			cursor.execute("DELETE FROM shop WHERE role_id = {}".format(role.id))
+			connection.commit()
+			await ctx.send( embed = emb )
+
+
+def setup(bot):
+	bot.add_cog(Shop(bot))
+	print('[COGS] Shop be loaded')
