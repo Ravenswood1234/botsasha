a2d8950127d4124e8871eb4f44197c2c5ae97f92
SashaKilin <66529400+SashaKilin@users.noreply.github.com>
2020-08-04 20:04:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -100,8 +100,8 @@ async def on_ready():
 
         cursor.execute("UPDATE users SET cost_kindcoin =  {} WHERE id = {}".format(costt, 558235304138637332))
         connection.commit()
-        await chanel.send(f"Стоимость KindCoins была изменена! Стоимость: {costt}")
-        print(f"Стоимость KindCoins была изменена!  Стоимость: {costt} Последнии изменения были {ttime} секунд назад то")
+        await chanel.send(f"Стоимость KindCoins была изменена! Стоимость: {costt} Последнии изменения были {ttime} секунд назад")
+        print(f"Стоимость KindCoins была изменена!  Стоимость: {costt} Последнии изменения были {ttime} секунд назад")
         await asyncio.sleep(ttime)
 
 #@bot.event
